349d981a1953b03503cc64b1fba229ba7c8a974b
Evan <evfernberg@gmail.com>
2021-07-14 15:26:21 -0400

    first commit

---
+++ schemas/comment.js
@@ -0,0 +1,16 @@
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: {type: 'author'},
+    },
+    {
+
+    },
+  ]
+};
